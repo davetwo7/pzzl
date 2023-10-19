3c7f2cd1b12205b851eaea8caee610f5bd9cba4f
davetwo7 <davidolu2019@gmail.com>
2023-10-19 17:59:03 -0400

    Finish FEN parser

--- pzzl/app/page.tsx
+++ pzzl/app/page.tsx
@@ -1,10 +1,14 @@
 import Board from "./components/Board";
+import parseFEN from "./utils/parseFEN";
 
 export default function Home() {
+  let test = parseFEN("r6k/pp2r2p/4Rp1Q/3p4/8/1N1P2R1/PqP2bPP/7K b - - 0 24");
+  console.log(test);
   return (
     <main className="flex items-center flex-col">
       <h1 className="p-8 text-2xl font-ranade-medium">pzzl.</h1>
       <Board />
+      <div>jelo</div>
     </main>
   );
 }
--- pzzl/app/utils/parseFEN.ts
+++ pzzl/app/utils/parseFEN.ts
@@ -1,22 +1,48 @@
 type Piece = 'r' | 'n' | 'b' | 'q' | 'k' | 'p' | 'R' | 'N' | 'B' | 'Q' | 'K' | 'P' | null;
 
-interface FEN {
+interface ChessState {
+  board: Piece[][];
+  turn: string;
+  castling: string;
+  enPassant: string;
+  halfMoveClock: number;
+  firstMoveTimer: number;
+};
 
-}
-
-interface ParsedFEN {
-
-}
-
-const parseFEN = (fen: String) => {
+const parseFEN = (fen: String): ChessState => {
   let parts = fen.split(" ");
   let ranks = parts[0].split("/");
 
-  const board: Piece[][] = ranks.map()
+  const board: Piece[][] = ranks.map((rank) => {
+    const row: Piece[] = [];
+    for (let i = 0; i < rank.length; i++) {
+      const char = rank[i];
+      if (isNaN(Number(char))) {
+        row.push(char as Piece)
+      } else {
+        for (let j = 0; j < Number(char); j++) {
+          row.push(null);
+        }
+      }
+    }
+    return row;
+  })
 
-  return 1;
+  const turn = parts[1] as ("w" | "b");
+  const castling = parts[2];
+  const enPassant = parts[3];
+  const halfMoveClock = parseInt(parts[4]);
+  const firstMoveTimer = parseInt(parts[5]);
+
+  console.log(parts)
+  return {
+    board,
+    turn,
+    castling,
+    enPassant,
+    halfMoveClock,
+    firstMoveTimer
+  };
 }
 
-parseFEN("r6k/pp2r2p/4Rp1Q/3p4/8/1N1P2R1/PqP2bPP/7K b - - 0 24")
-
-// export default parseFEN;
+export default parseFEN;
